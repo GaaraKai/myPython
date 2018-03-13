10b62c0ee2e3375b28d69a227128171099bd3eca
GaaraKai <wangrongkai0202@163.com>
2018-03-13 10:35:14 +0800

    20180313 10:23 提交 删除不需要的方法susp_device_stat/susp_mer_stat

--- riskproject/real/config.py
+++ riskproject/real/config.py
@@ -7,8 +7,8 @@ class DeviceConfig:
     RESULT_PATH = "D:/github_program/myPython/docs/rst/"
     PND_TD_DEVICE_FILE = "D:/github_program/myPython/docs/csvfiles/device_bl_analysis/pnd_td_device.csv"
     START_DATE = "2018-01-01"
-    END_DATE = "2018-01-31"
+    END_DATE = "2018-01-05"
     DATE_RANGE = (datetime.datetime.strptime(END_DATE, '%Y-%m-%d')
-                  - datetime.datetime.strptime(START_DATE, '%Y-%m-%d')).days
+                  - datetime.datetime.strptime(START_DATE, '%Y-%m-%d')).days + 1
     ID_HURDLE = 30
     TRX_HURDLE = 60000
--- riskproject/real/device_bl_analysis.py
+++ riskproject/real/device_bl_analysis.py
@@ -11,7 +11,7 @@ import tkinter.messagebox as tm
 import numpy as np
 import pandas as pd
 
-from riskproject.test.core import config
+from riskproject.real import config
 
 
 class Logger:
@@ -79,98 +79,6 @@ def get_trx_detail(parm_reader):
     rtn_df = rtn_df[(rtn_df["plat_date"] >= conf.START_DATE) & (rtn_df["plat_date"] <= conf.END_DATE)]
 
     return rtn_df
-
-
-def susp_device_stat(parm_df):
-    logger.info("****SUSPICIOUS DEVICE ANALYSIS BEGIN****")
-    logger.info("[1]. ALL TRANSACTION WITH DROP NULL ID_NO[SHAPE]:")
-    rst_dropna_id_no = parm_df.dropna(axis=0, subset=["id_no"])
-    logger.info(rst_dropna_id_no.shape)
-    logger.info("[2]. AFTER [1], DROP DUPLICATED TD_DEVICE & ID_NO[SHAPE]:")
-    rst_dropna_id_no_dup = rst_dropna_id_no.drop_duplicates(["td_device", "id_no"])
-    logger.info(rst_dropna_id_no_dup.shape)
-    logger.info("[3]. FIND COUNT OF ID_NO BY EVERY DEVICE[TOP5]:")
-    devi_with_id_cnt = pd.pivot_table(rst_dropna_id_no_dup, index=["td_device"], values=["id_no"], aggfunc=len) \
-        .sort_values(by='id_no', axis=0, ascending=False).reset_index()
-    devi_with_id_cnt.rename(columns={"id_no": "id_no_cnt"}, inplace=True)
-    logger.info(devi_with_id_cnt.head())
-    logger.info("[4]. FIND SUSPICIOUS DEVICE WHERE ID HURDLE IS[%s]:" % conf.ID_HURDLE)
-    susp_device_list = pd.Series(devi_with_id_cnt[devi_with_id_cnt["id_no_cnt"] >= conf.ID_HURDLE]["td_device"]) \
-        .tolist()
-    if len(susp_device_list) == 0:
-        logger.info("NO SUSPICIOUS DEVICE BECAUSE ID HURDLE %s IS TOO HIGH." % conf.ID_HURDLE)
-    else:
-        logger.info("==>> SUSPICIOUS DEVICE: %s" % susp_device_list)
-        logger.info("[5]. FIND MERCHANT WHIT SUSPICIOUS DEVICE:")
-        mer_with_susp_device = pd.DataFrame(
-            rst_dropna_id_no_dup[rst_dropna_id_no_dup["td_device"].isin(susp_device_list)])
-        mer_with_susp_device_list = mer_with_susp_device["mer_id"].unique().tolist()
-        logger.info("==>> SUSPICIOUS MERCHANT: %s" % mer_with_susp_device_list)
-        mer_with_susp_device_detl = pd.pivot_table(mer_with_susp_device,
-                                                   values=["inst_id"],
-                                                   index=["mer_id", "td_device", "id_no"],
-                                                   aggfunc=len).sort_values(by='inst_id', axis=0,
-                                                                            ascending=False).reset_index()
-        logger.info("[6]. FIND SUSPICIOUS DEVICE TRANSACTION DETAILS:")
-        logger.info("==>> SUSPICIOUS TRANSACTION[e.g.]:")
-        logger.info(mer_with_susp_device_detl.head())
-        logger.info("[7]. SAVE SUSPICIOUS DEVICE TRANSACTION DETAILS:")
-        mer_with_susp_device_trx_details_path = conf.RESULT_PATH + "mer_with_susp_device_trx_details" + ".csv"
-        # mer_with_susp_device.to_csv(mer_with_susp_device_trx_details_path, index=False)
-        logger.info("SAVE TO: %s" % mer_with_susp_device_trx_details_path)
-    logger.info("****SUSPICIOUS DEVICE ANALYSIS END****")
-    del parm_df
-
-
-def susp_mer_stat(parm_rst):
-    # global RST_PATH, TRX_HURDLE
-    logger.info("****SUSPICIOUS MERCHANT ANALYSIS BEGIN****")
-    logger.info("[1]. FIND MERCHANT TRANSACTION COUNT[TOP5]:")
-    mer_with_trx_cnt = pd.pivot_table(parm_rst, index=["mer_id"], values=["inst_id"], aggfunc=len) \
-        .sort_values(by='inst_id', axis=0, ascending=False).reset_index()
-    mer_with_trx_cnt.rename(columns={"inst_id": "trx_cnt"}, inplace=True)
-    logger.info(mer_with_trx_cnt.head())
-
-    logger.info("[2]. FIND MERCHANT DEVICE COUNT[TOP5]:")
-    rst_drop_mer_td = parm_rst.drop_duplicates(["mer_id", "td_device"])
-    mer_with_td_device_cnt = pd.pivot_table(rst_drop_mer_td, index=["mer_id"], values=["td_device"],
-                                            aggfunc=len) \
-        .sort_values(by='td_device', axis=0, ascending=False).reset_index()
-    mer_with_td_device_cnt.rename(columns={"td_device": "device_cnt"}, inplace=True)
-    logger.info(mer_with_td_device_cnt.head())
-    logger.info("[3]. MERGE TRANSACTION & DEVICE COUNT BY MERCHANT ID[TOP5]:")
-    merge_rst = pd.merge(mer_with_trx_cnt, mer_with_td_device_cnt, how='inner', on=['mer_id'])
-    merge_rst["avg_trx_cnt_by_one_device"] = \
-        round(merge_rst["trx_cnt"] / merge_rst["device_cnt"], 0)
-    logger.info(merge_rst.head())
-    logger.info("SHAPE:")
-    logger.info(merge_rst.shape)
-    logger.info("[4]. SAVE MERCHANT TRANSACTION & DEVICE COUNT STATISTICAL TABLE:")
-    mer_with_susp_device_trx_details_path = conf.RESULT_PATH + "mer_with_susp_device_trx_details" + ".csv"
-    # mer_with_susp_device.to_csv(mer_with_susp_device_trx_details_path, index=False)
-    logger.info("SAVE TO: %s" % mer_with_susp_device_trx_details_path)
-    logger.info("[5] FIND MERCHANT WHERE TRANSACTION COUNT OVER HURDLE[%s]:" % conf.TRX_HURDLE)
-    key_mer_by_trx_list = pd.Series(
-        mer_with_trx_cnt[mer_with_trx_cnt["trx_cnt"] >= conf.TRX_HURDLE]["mer_id"]).tolist()
-    if len(key_mer_by_trx_list) == 0:
-        logger.info("==>> NO MERCHANT BECAUSE TRX_HURDLE [%s] IS TOO HIGH." % conf.TRX_HURDLE)
-    else:
-        logger.info("==>> MONITOR MERCHANT COUNT: %s" % len(key_mer_by_trx_list))
-        logger.info("==>> MONITOR MERCHANT DETAILS: %s" % key_mer_by_trx_list)
-        logger.info("==>> MONITOR MERCHANT TRANSACTION COUNT[OVER HURDLE]:")
-        key_mer_trx_stat = pd.DataFrame(merge_rst[merge_rst["mer_id"].isin(key_mer_by_trx_list)])
-        logger.info(key_mer_trx_stat)
-    avg_trx_top = 5
-    logger.info("[6] FIND MERCHANT WHERE AVERAGE TRANSACTION COUNT WITH DEVICE[TOP %s]:" % avg_trx_top)
-    key_mer_avg_stat = pd.DataFrame(
-        merge_rst.sort_values(by='avg_trx_cnt_by_one_device', axis=0, ascending=False))[0:avg_trx_top]
-    key_mer_by_avg_list = pd.Series(key_mer_avg_stat["mer_id"]).tolist()
-    logger.info("==>> SUSPICIOUS MERCHANT COUNT: %s" % (len(key_mer_by_avg_list)))
-    logger.info("==>> SUSPICIOUS MERCHANT ID: %s" % key_mer_by_avg_list)
-    logger.info("==>> SUSPICIOUS MERCHANT TRANSACTION COUNT[TOP5]:")
-    logger.info(key_mer_avg_stat)
-    logger.info("****SUSPICIOUS MERCHANT ANALYSIS END****")
-    del parm_rst
 
 
 def overall_rpt(parm_rst):
@@ -252,7 +160,7 @@ def data_preproc(parm_csv_folder, parm_csv_file_list):
         # 1. 获取CSV文件路径
         csv_file_path = os.path.join('%s%s%s' % (parm_csv_folder, '/', csv_file))
         logger.info("csv_file_path = %s" % csv_file_path)
-        csv_file_path = "D:/github_program/myPython/docs/csvfiles/201801/td_device_201801"
+        # csv_file_path = "D:/github_program/myPython/docs/csvfiles/201801/td_device_201801"
         # csv_file_path = "D:/github_program/myPython/docs/csvfiles/todo_td/NO 1_td_1"
 
         # 2. 读取CSV文件
@@ -275,8 +183,8 @@ def data_preproc(parm_csv_folder, parm_csv_file_list):
 
 
 def get_csv_folder():
-    csv_folder = "D:/github_program/myPython/docs/csvfiles/todo_td/"
-    # csv_folder = tf.askdirectory(title=u"选择文件CSV文件夹", initialdir=conf.CSV_PATH)
+    # csv_folder = "D:/github_program/myPython/docs/csvfiles/todo_td/"
+    csv_folder = tf.askdirectory(title=u"选择文件CSV文件夹", initialdir=conf.CSV_PATH)
     if len(csv_folder) == 0:
         tm.showinfo(title='提示', message='请选取的CSV文件夹')
         sys.exit(0)
@@ -290,14 +198,9 @@ def create_rpt(parm_rst):
         # 1. OVERALL REPORT
         overall_rpt(parm_rst)
         logger.info("--------------------------------------------")
-        # 2. SUSPICIOUS DEVICE ANALYSIS
-        # susp_device_stat(parm_rst)
-        logger.info("--------------------------------------------")
-        # 3. SUSPICIOUS MERCHANT ANALYSIS
-        # susp_mer_stat(parm_rst)
-        logger.info("--------------------------------------------")
-        # 4. DEVICE ANALYSIS
+        # 2. DEVICE ANALYSIS
         device_analysis(parm_rst)
+        logger.info("--------------------------------------------")
     else:
         logger.info("parm_rst IS NULL, WRONG...")
     del parm_rst
@@ -334,8 +237,6 @@ def init():
     logger.info("START DATE: %s" % conf.START_DATE)
     logger.info("END DATE: %s" % conf.END_DATE)
     logger.info("DATE RANGE: %s" % conf.DATE_RANGE)
-    logger.info("ID HURDLE: %s" % conf.ID_HURDLE)
-    logger.info("TRX HURDLE: %s" % conf.TRX_HURDLE)
     logger.info("--------------------------------------------")
 
 
