cfc627859f6da5066421f4fe0c6086d51992abd4
GaaraKai <wangrongkai0202@163.com>
2018-04-27 17:42:50 +0800

    20180427 17:39 提交 商户相似度制图+配置文件

--- riskproject/real/config.py
+++ riskproject/real/config.py
@@ -1,4 +1,5 @@
 import datetime
+import pandas as pd
 
 
 class DeviceConfig:
@@ -6,9 +7,22 @@ class DeviceConfig:
     CSV_PATH = "D:/github_program/myPython/docs/csvfiles"
     CONF_PATH = "D:/github_program/myPython/docs/conf/"
     RESULT_PATH = "D:/github_program/myPython/docs/rst/"
-    START_DATE = "2018-01-01"
-    END_DATE = "2018-03-10"
+    START_DATE = "2018-04-01"
+    END_DATE = "2018-04-20"
     DATE_RANGE = (datetime.datetime.strptime(END_DATE, '%Y-%m-%d')
                   - datetime.datetime.strptime(START_DATE, '%Y-%m-%d')).days + 1
+    START_DATE = pd.Timestamp(2018, 4, 1)
+    END_DATE = pd.Timestamp(2018, 4, 20)
+    DATE_RANGE = END_DATE - START_DATE
     ID_HURDLE = 30
     TRX_HURDLE = 60000
+
+
+class SimilarityConfig:
+    LOG_PATH = "D:/github_program/myPython/logs/logs.log"
+    CSV_PATH = "D:/github_program/myPython/docs/csvfiles"
+    CONF_PATH = "D:/github_program/myPython/docs/conf/"
+    RESULT_PATH = "D:/github_program/myPython/docs/rst/"
+    START_DATE = pd.Timestamp(2018, 4, 1)
+    END_DATE = pd.Timestamp(2018, 4, 20)
+    DATE_RANGE = END_DATE - START_DATE
